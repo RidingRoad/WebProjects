836b5b6094014df1cf0097d6329289d5c8def77b
RidingRoad <RidingRoad@163.com>
2018-06-28 14:19:10 +0800

    modified the sms and image_store files

--- Flask_Learning/News_Website/info/utils/image_storage.py
+++ Flask_Learning/News_Website/info/utils/image_storage.py
@@ -1,8 +1,8 @@
 from qiniu import Auth, put_data
 
-access_key = "yV4GmNBLOgQK-1Sn3o4jktGLFdFSrlywR2C-hvsW"
-secret_key = "bixMURPL6tHjrb8QKVg2tm7n9k8C7vaOeQ4MEoeW"
-bucket_name = "ihome"
+access_key = "XXXX"
+secret_key = "XXXXXXX"
+bucket_name = "XXXXX"
 
 
 def storage(data):
--- Flask_Learning/News_Website/info/utils/yuntongxun/sms.py
+++ Flask_Learning/News_Website/info/utils/yuntongxun/sms.py
@@ -3,13 +3,13 @@
 from .CCPRestSDK import REST
 
 # 说明：主账号，登陆云通讯网站后，可在"控制台-应用"中看到开发者主账号ACCOUNT SID
-_accountSid = '8a216da863cde6f90163f3105ed21100'
+_accountSid = 'xxxx'
 
 # 说明：主账号Token，登陆云通讯网站后，可在控制台-应用中看到开发者主账号AUTH TOKEN
-_accountToken = '28355601152b41a28a91662fe538d682'
+_accountToken = 'xxxxx'
 
 # 请使用管理控制台首页的APPID或自己创建应用的APPID
-_appId = '8a216da863cde6f90163f3105f241106'
+_appId = 'xxxxx'
 
 # 说明：请求地址，生产环境配置成app.cloopen.com
 _serverIP = 'sandboxapp.cloopen.com'
